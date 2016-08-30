69019e91cb2c51b198b942339fa4721186f42d77
rahalkanishka <rahalncm@gmail.com>
2016-08-30 23:27:08 +0530

    Added mysql-java connector and connection class

--- PayRolleSystem/src/Views/Login.java
+++ PayRolleSystem/src/Views/Login.java
@@ -5,6 +5,8 @@
  */
 package Views;
 
+import Details.javaconnect;
+
 /**
  *
  * @author Rahal
@@ -16,6 +18,7 @@ public class Login extends javax.swing.JFrame {
      */
     public Login() {
         initComponents();
+        javaconnect.connect();
     }
 
     /**
